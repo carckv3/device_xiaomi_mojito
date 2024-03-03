f7eece83485be091ad563995b52c2ec712601c0d
carckv3 <157272614+carckv3@users.noreply.github.com>
2024-03-03 01:49:07 -0800

    new ify for simple

---
+++ vendorsetup.sh
@@ -0,0 +1,8 @@
+# clang
+git clone --depth=1 https://github.com/DrtSinX98/android_prebuilts_clang_host_linux-x86_clang-6443078 prebuilts/clang/host/linux-x86/clang-6443078
+
+# Vendor tree
+git clone https://github.com/BootleggersROM-Devices/vendor_xiaomi_mojito.git -b  urubino vendor/xiaomi/mojito --depth=1
+
+#kernel tree
+git clone https://github.com/BootleggersROM-Devices/kernel_xiaomi_mojito.git kernel/xiaomi/mojtio --depth=1
