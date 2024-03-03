2acf5532b79e2f366d0b6ef24b2e9a074bb419d2
carckv3 <157272614+carckv3@users.noreply.github.com>
2024-03-03 01:39:45 -0800

    banana-ify

--- banana_mojito.mk
+++ banana_mojito.mk
@@ -11,21 +11,26 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from mojito device
 $(call inherit-product, device/xiaomi/mojito/device.mk)
 
-# Inherit some common Bootleggers ROM stuff.
-$(call inherit-product, vendor/bootleggers/config/common_full_phone.mk)
+# Inherit some common Banana ROM stuff.
+$(call inherit-product, vendor/banana/config/common.mk)
 
 # Product Specifics
-PRODUCT_NAME := bootleg_mojito
+PRODUCT_NAME := banana_mojito
 PRODUCT_DEVICE := mojito
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 10
 PRODUCT_MANUFACTURER := Xiaomi
 
-BOOTLEGGERS_BUILD_TYPE := Shishufied
-DEVICE_MAINTAINERS := Rom Bdr. Gharti
+# Build configuration
+IS_PHONE := true
+BANANA_BUILD_TYPE := OFFICIAL
+BANANA_MAINTAINER := NINJA
+WITH_GAPPS := true
+TARGET_SUPPORT_QUICK_TAP := true
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_GAPPS_ARCH := arm64
 
 # Some Build Flags
-TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_INCLUDE_PIXEL_CHARGER := true
 
